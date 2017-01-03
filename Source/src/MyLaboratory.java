7e69fae58c0f50931e8374248cdd9584cb41b6bb
Sylvain Hallé <shalle@acm.org>
2017-01-03 10:46:44 -0500

    Updated project

---
+++ Source/src/MyLaboratory.java
@@ -0,0 +1,13 @@
+import ca.uqac.lif.labpal.Laboratory;
+
+public class MyLaboratory extends Laboratory
+{
+	
+	public static void main(String[] args)
+	{
+		// Nothing else to do here
+		MyLaboratory.initialize(args, MyLaboratory.class);
+	}
+	
+	
+}
